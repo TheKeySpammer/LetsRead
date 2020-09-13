a9a73eaa078a9a0c4efb5b0e9251161e9108e433
theKeySpammer <gt_ams@yahoo.in>
2020-09-13 23:58:19 +0530

    remove sync

--- app.js
+++ app.js
@@ -56,12 +56,12 @@ db.sequelize.authenticate()
 const models = require('./models/Models');
 
 // Sync DB
-db.sequelize.sync({force: true}).then(() => {
-  require('./modules/seed')();
-  console.log("Database synchronized");
-}).catch(err => {
-  console.log("Error while synchronizing data: ", err);
-});
+// db.sequelize.sync({force: true}).then(() => {
+//   require('./modules/seed')();
+//   console.log("Database synchronized");
+// }).catch(err => {
+//   console.log("Error while synchronizing data: ", err);
+// });
 
 global.dbErrorMsg = "Database not responding try again later";
 global.cookieOpt = {
